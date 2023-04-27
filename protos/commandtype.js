e99434a51b150a2189cd72fb46e5d88cf30e6116
cry1s <cry1s@ya.ru>
2023-04-27 13:28:50 +0300

    generated code

---
+++ protos/commandtype.js
@@ -0,0 +1,24 @@
+// source: MessageEnums.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.ru.sovcombank.hackaton.proto.CommandType');
+
+/**
+ * @enum {number}
+ */
+proto.ru.sovcombank.hackaton.proto.CommandType = {
+  CTHANDSHAKE: 0,
+  CTSTATUS: 1,
+  CTEXECCOMMAND: 2
+};
+
