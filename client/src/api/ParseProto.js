57485fc96df2f7e3be52a7bd45ae183aff5b899b
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-04-29 19:23:26 +0300

    asd

--- client/src/api/ParseProto.js
+++ client/src/api/ParseProto.js
@@ -6,9 +6,5 @@ const EMProto = proto.ExchangeInfoMessage;
 export function ProtoMessageDecoder(messageBuffer) {
     const buffer = Buffer.from(messageBuffer);
     const message = EMProto.decode(buffer);
-<<<<<<< HEAD
-
-=======
->>>>>>> da3e4f47e9fe03cee88ec64936bdaa9329896d07
     return message;
 }
