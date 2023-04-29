e95d656867cb7e99e0f063e4edf928b0611f7b50
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-04-30 02:51:36 +0300

    sad

--- client/src/Components/Connection/Response/Response.js
+++ client/src/Components/Connection/Response/Response.js
@@ -16,12 +16,13 @@ const ResponseBody = observer(() => {
     const [details, setDetails] = useState("undefined");
     const [nextTime, setNextTime] = useState("undefined");
     const [showGraph, setShowGraph] = useState(false);
+    const [showGraphRes, setShowGraphRes] = useState(false);
 
     useEffect(() => {
         Socket.socket.on("sentBrokerTable", (data) => {
             setDetails(getDetails(data));
             console.log(OnRecieve(data));
-            setNextTime(getNextTime(data) - Date.now());
+            setNextTime(getNextTime(data) - Date.now() + " ms");
             Status.setStatus(getType(data));
 
             data = ProtoMessageDecoder(data);
@@ -49,6 +50,45 @@ const ResponseBody = observer(() => {
             </div>
             <div className="responsepgContent">
                 {!showGraph ?
+                    <table>
+                    <thead>
+                        <tr>
+                            {Array.from({ length: cols }).map((_, index) =>
+                                    (!data.advStatus.fields[index].caption) ? {} :
+                                        <th key={index}>{data.advStatus.fields[index].caption}</th>
+                            )}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Array.from({ length: rows }).map((_, rowIndex) => (
+                            <tr key={rowIndex}>
+                                {Array.from({ length: cols }).map((_, colIndex) => (
+                                    <td key={colIndex}>
+                                        { (!data.advStatus.data.rows[rowIndex].values[colIndex]) ? null :
+                                            data.advStatus.data.rows[rowIndex].values[colIndex].value.value
+                                        }
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                :
+                <Graphics data={data}/>}
+            </div>
+            <div className="resTimeContainer">
+                <p className="responseTimeEvent">Задержка события таблицы: <span>{resTimeEvent}</span></p>
+            </div>
+            <div className="information-container">
+                <p className="details">Детали последнего обновления: <span>{details}</span></p>
+                <p className="next-time">next time: <span>{nextTime}</span></p>
+                {!showGraphRes ?
+                <button className="createGrpahics" onClick={() => setShowGraphRes(!showGraphRes)}>Создать график</button>
+                :
+                <button className="closeGraphics" onClick={() => setShowGraphRes(!setShowGraphRes)}>Показать таблицу</button>}
+            </div>
+            <div className="responsepgContent">
+                {!showGraphRes ?
                     <table>
                     <thead>
                         <tr>
