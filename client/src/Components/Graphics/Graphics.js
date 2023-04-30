74229892835fee80a1ae9571904b814015a04274
Vanv1k <ivanspecial5@gmail.com>
2023-04-30 05:32:38 +0300

    better graph

--- client/src/Components/Graphics/Graphics.js
+++ client/src/Components/Graphics/Graphics.js
@@ -19,7 +19,7 @@ const renderLineChart = (
   <LineChart width={800} height={500} data={data}>
     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
     <XAxis dataKey="name" />
-    <YAxis />
+    <YAxis domain={[0,Math.round(Math.max(colValues.data))+1]} />
   </LineChart>
 );
     return (
