b2469083b55b79d5407f1ee4fb0ac70f51056f55
Vanv1k <ivanspecial5@gmail.com>
2023-04-30 07:42:23 +0300

    disable started select

--- client/src/Components/Connection/Response/Response.js
+++ client/src/Components/Connection/Response/Response.js
@@ -34,7 +34,7 @@ const ResponseBody = observer(() => {
                 <p className="next-time">next time: <span>{showNextTime}</span></p>
                 <div className="graph">
                     <select className="select" onChange={(e) => setOption(e.target.value)}>
-                        <option key="default" value="choose option">Выберите опцию</option>
+                        <option key="default" value="choose option" selected="true" disabled="disabled">Выберите опцию</option>
                         {table.fields.filter((field, i) => (table.types[i] === 1 || table.types[i] === 2)).map((field, i) => (
                             <option value={i} key={i}>{field}</option>
                         ))}
