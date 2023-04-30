32e0261d8dc9d117607444a36af66b37bb49a6e4
cry1s <cry1s@ya.ru>
2023-04-30 09:00:53 +0300

    commit

--- exchange_service/index.js
+++ exchange_service/index.js
@@ -215,7 +215,7 @@ function randomString(length) {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
-    for (let i = 0; i < charactersLength; i++)        
+    for (let i = 0; i < length; i++)        
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     return result;
 }
